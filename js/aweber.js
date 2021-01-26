191f26e7492b84e390cfce8c14f4b4fe6e9b2e7c
Markus Mattsson <markusmattsson90@gmail.com>
2021-01-26 20:27:42 +0100

    Change form styling

--- js/aweber.js
+++ js/aweber.js
@@ -1,22 +1,3 @@
-// Special handling for in-app browsers that don't always support new windows
-(function() {
-    function browserSupportsNewWindows(userAgent) {
-        var rules = [
-            'FBIOS',
-            'Twitter for iPhone',
-            'WebView',
-            '(iPhone|iPod|iPad)(?!.*Safari\/)',
-            'Android.*(wv|\.0\.0\.0)'
-        ];
-        var pattern = new RegExp('(' + rules.join('|') + ')', 'ig');
-        return !pattern.test(userAgent);
-    }
-
-    if (!browserSupportsNewWindows(navigator.userAgent || navigator.vendor || window.opera)) {
-        document.getElementById('af-form-791915812').parentElement.removeAttribute('target');
-    }
-})();
-
 (function() {
     var IE = /*@cc_on!@*/false;
     if (!IE) { return; }
